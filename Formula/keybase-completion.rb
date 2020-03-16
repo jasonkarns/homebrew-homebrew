b7a60d86b51379c9687e10ed191f685c0bd8f530
Jason Karns <jason.karns@gmail.com>
2020-03-15 22:38:24 -0400

    Fix lint

--- Formula/keybase-completion.rb
+++ Formula/keybase-completion.rb
@@ -2,8 +2,8 @@ class KeybaseCompletion < Formula
   desc "Bash completion for Keybase"
   homepage "https://github.com/keybase/client/pull/11261"
   url "https://raw.githubusercontent.com/keybase/client/495ff6aeb0bb71da2539641b082faf98b85210dd/completion/bash"
-  sha256 "bbbe0a6f9d3dce86d5656907b7598f34ac3841e176c271cef14516af4df7f4fa"
   version "1.0.1"
+  sha256 "bbbe0a6f9d3dce86d5656907b7598f34ac3841e176c271cef14516af4df7f4fa"
 
   bottle :unneeded
 
