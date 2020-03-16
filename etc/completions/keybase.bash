17677e8ae137f3e4723e002d7c6dd8bcd8b29a87
Jason Karns <jason.karns@gmail.com>
2020-03-15 21:58:43 -0400

    Create keybase completion

---
+++ etc/completions/keybase.bash
@@ -0,0 +1,11 @@
+# from https://github.com/keybase/client/pull/11261
+
+_keybase()
+{
+    COMPREPLY=()
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    completions=$(${COMP_WORDS[@]:0:$COMP_CWORD} --generate-bash-completion)
+    COMPREPLY=( $(compgen -W "$completions" -- "$cur") )
+    return 0
+}
+complete -F _keybase keybase
