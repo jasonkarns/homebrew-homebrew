fce046623087b158522a013ce0ad30354ae29c2d
Jason Karns <jason.karns@gmail.com>
2020-08-20 10:27:01 -0400

    Adds dotnet cli completion
    see https://github.com/dotnet/sdk/issues/4674
    https://docs.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete

---
+++ Formula/dotnet-completion.rb
@@ -0,0 +1,18 @@
+class DotnetCompletion < Formula
+  desc "Bash completion for dotnet cli"
+  homepage "https://github.com/dotnet/sdk"
+  url "https://raw.githubusercontent.com/dotnet/sdk/e80cf181f715697b88f18845d095bb9b623f325a/scripts/register-completions.bash"
+  version "3.1.7"
+  sha256 "90c1931ceb1f91b3535147761668b854e90c042a7fc138b7fec08b8a18961afc"
+  license "MIT"
+
+  bottle :unneeded
+  def install
+    bash_completion.install "register-completions.bash" => "dotnet"
+  end
+
+  test do
+    assert_match "complete -f -F _dotnet_bash_complete dotnet",
+                 shell_output("source #{bash_completion}/dotnet && complete -p dotnet")
+  end
+end
