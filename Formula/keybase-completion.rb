2dd561cb4ce76945ff2bbdb2d98261f610518a9d
Jason Karns <jason.karns@gmail.com>
2021-12-23 15:07:32 +0000

    keybase-completion: add 1.0.1_1 bottle.

--- Formula/keybase-completion.rb
+++ Formula/keybase-completion.rb
@@ -6,6 +6,11 @@ class KeybaseCompletion < Formula
   sha256 "bbbe0a6f9d3dce86d5656907b7598f34ac3841e176c271cef14516af4df7f4fa"
   revision 1
 
+  bottle do
+    root_url "https://github.com/jasonkarns/homebrew-homebrew/releases/download/keybase-completion-1.0.1_1"
+    sha256 cellar: :any_skip_relocation, big_sur: "64898001fd4667e2c437fa5bf96df6d65dbbe76697a5f1486bf9414c38a9729e"
+  end
+
   def install
     bash_completion.install "bash" => "keybase"
   end
